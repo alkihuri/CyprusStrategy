e6b18f2af7dc4f411da2d8b019fc4fdda86df250
robolab-unity <71641579+robolab-unity@users.noreply.github.com>
2021-02-25 00:27:14 +0300

    DrawtOfLesson

---
+++ CyprusStrategyUntiyProject/Assets/Scripts/City.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class City : MonoBehaviour
+{
+    public string cityName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cityName = name;
+    }
+
+     private void OnTriggerExit(Collider collider)
+     {
+         collider.gameObject.GetComponentInChildren<Light>().color = Color.red;
+         Debug.Log(name + " is passed");
+     }
+
+     private void OnTriggerEnter(Collider collider)
+     {
+         collider.gameObject.GetComponentInChildren<Light>().color = Color.green;
+         Debug.Log("Arrived to " + name);
+     }
+}
---
+++ CyprusStrategyUntiyProject/Assets/Scripts/DeleveryBoyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class DeleveryBoyController : MonoBehaviour
+{
+
+
+    NavMeshAgent navigation; 
+    GameManager gameManager; 
+    [Range(0,6)]public float offset  = 3;
+      Vector3 randomCity ; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+        navigation = GetComponent<NavMeshAgent>();
+        randomCity = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+        if(Vector3.Distance(transform.position , randomCity) < offset || navigation.velocity.magnitude<0.1f)
+        {
+            randomCity = gameManager.cities[Random.Range(0,gameManager.cities.Count)].transform.position;
+        }
+        navigation.SetDestination(randomCity);
+    }
+}
